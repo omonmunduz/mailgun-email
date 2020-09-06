7fb4713e14adfe54c5db771687852a2c2afb56df
omonmunduz <omurbekn@gmail.com>
2020-09-06 10:35:06 -0700

    minor

--- functions/contact.js
+++ functions/contact.js
@@ -10,7 +10,7 @@ exports.handler = (event, _context, callback) => {
   const data = JSON.parse(event.body);
 
   const emailData = {
-    from: 'Omur Nazar <omurbekn@gmail.com>',
+    from: 'Omurbek Nazarov <omurbekn@gmail.com>',
     to: `${data.name} <${data.email}>`,
     subject: data.subject,
     text: data.body
